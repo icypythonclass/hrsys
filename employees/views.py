861ca5f0ec4d779e58089c52c9e4b8011e1c477c
李超逸 <icylydia@gmail.com>
2018-06-17 12:59:38 +0900

    layout

--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,24 @@
-from material.frontend.views import ModelViewSet
+from material.frontend.views import ModelViewSet, CreateModelView
+from material import Layout, Row
+from django.views.generic import CreateView
 
 from . import models
 
 
+class EmployeeCreateView(CreateModelView):
+    layout = Layout(
+        Row('birth_date'),
+        Row('last_name', 'first_name', 'gender',),
+        Row('hire_date'),
+    )
+
+    def has_add_permission(self, request):
+        return True
+
+
 class EmployeeViewSet(ModelViewSet):
     model = models.Employee
+    create_view_class = EmployeeCreateView
 
 
 class AttendanceViewSet(ModelViewSet):
