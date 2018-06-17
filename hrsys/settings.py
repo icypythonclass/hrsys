086f169a9afe94086b0610e65278a850ce10f8b4
呂麗莎 <>
2018-06-17 11:59:59 +0900

    bug fix

--- hrsys/settings.py
+++ hrsys/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'employees.apps.EmployeesConfig',
     'CareerHistory.apps.CareerHistoryConfig',
+    'cases.apps.CasesConfig',
 ]
 
 MIDDLEWARE = [
